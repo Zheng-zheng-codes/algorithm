112e38734bbe9bc7b56c2c5f06c2e3111f90d7c7
Zheng-zheng-codes <2429250082@qq.com>
2025-11-20 20:46:26 +0800

    Add files via upload

---
+++ 图论/图的连通性/割点.cpp
@@ -0,0 +1,65 @@
+//https://www.luogu.com.cn/problem/P3388
+#include<bits/stdc++.h>
+using namespace std;
+const int maxn=2e4+10;
+vector<int>tu[maxn];
+bool jud[maxn];//判断是否遍历过
+int dfn[maxn];//dfs树的序列号
+int low[maxn];//当前节点和子树只往回走一次能走到的最小dfn
+bool iscut[maxn];//判断是不是割点
+int cnt;
+void init(int n)
+{
+    cnt=1;
+    for(int i=1;i<=n;i++)
+    {
+        tu[i].clear();
+        jud[i]=0;
+        iscut[i]=0;
+    }
+}
+void dfs(int v,int fa)
+{
+    dfn[v]=low[v]=cnt++;//low先定为dfn
+    jud[v]=1;
+    int n=0;
+    for(int i=0;i<tu[v].size();i++)
+    {
+        int nxt=tu[v][i];
+        if(!jud[nxt])
+        {
+            n++;
+            dfs(nxt,v);
+            low[v]=min(low[v],low[nxt]);//后面的节点往回走了一次
+            if(fa!=-1&&low[nxt]>=dfn[v])iscut[v]=1;
+            //当有一个子树往回走不过当前节点那么这个点连接它和前面的点，所以说去掉这个点就不连通了
+        }
+        else if(nxt!=fa)low[v]=min(low[v],dfn[nxt]);//自己往回走一次
+    }
+    if(fa==-1&&n>=2)iscut[v]=1;
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0),cout.tie(0);
+    int n,m;cin>>n>>m;
+    init(n);
+    while(m--)
+    {
+        int a,b;cin>>a>>b;
+        tu[a].push_back(b);
+        tu[b].push_back(a);
+    }
+    vector<int>ans;
+    for(int i=1;i<=n;i++)
+    {
+        if(!jud[i])dfs(i,-1);
+    }
+    for(int i=1;i<=n;i++)
+    {
+        if(iscut[i])ans.push_back(i);
+    }
+    sort(ans.begin(),ans.end());
+    cout<<ans.size()<<'\n';
+    for(int i=0;i<ans.size();i++)cout<<ans[i]<<' ';
+}
